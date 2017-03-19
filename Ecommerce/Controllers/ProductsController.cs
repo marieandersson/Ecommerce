0949584c4f11e867d3eaf595ac1348c91469ebfc
Marie Eriksson <eriksson.km@gmail.com>
2017-03-19 17:51:54 +0100

    add products database and setup controller with views

--- Ecommerce/Controllers/ProductsController.cs
+++ Ecommerce/Controllers/ProductsController.cs
@@ -3,15 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ecommerce.Models;
+using Dapper;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace Ecommerce.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["EcommerceDatabase"].ConnectionString;
         // GET: Products
         public ActionResult Index()
         {
-            return View();
+            List<ProductsViewModel> Products;
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                Products = connection.Query<ProductsViewModel>("select * from Products").ToList();
+            }
+            return View(Products);
+        }
+
+        public ActionResult Details(string id)
+        {
+            ProductsViewModel singleProduct;
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                var query = "select * from Products where id = @productId";
+                var parameters = new { productId = id };
+                singleProduct = connection.QuerySingleOrDefault<ProductsViewModel>(query, parameters);
+            }
+            if (singleProduct == null)
+            {
+                return HttpNotFound();
+            }
+            return View(singleProduct);
         }
     }
+
 }
---
+++ Ecommerce/Models/ProductsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.Models
+{
+    public class ProductsViewModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Format { get; set; }
+        public string Description { get; set; }
+        public string ImgUrl { get; set; }
+        public string Price { get; set; }
+        public string Stock { get; set; }
+        public DateTime Released { get; set; }
+    }
+}
