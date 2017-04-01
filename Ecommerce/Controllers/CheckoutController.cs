b8b45e69b0d8e32e32562ed7693226b28918f529
Marie Eriksson <eriksson.km@gmail.com>
2017-04-02 00:28:55 +0200

    add transaction to handle exeptions on checkout order

--- Ecommerce/Controllers/CheckoutController.cs
+++ Ecommerce/Controllers/CheckoutController.cs
@@ -39,54 +39,105 @@ namespace Ecommerce.Controllers
         public ActionResult SubmitOrder(string firstName, string lastName, string email, string phone, string street, string postalCode, string city)
         {
             var cartId = Request.Cookies["CartId"].Value;   
-            List<CheckoutViewModel> Products;
-            int OrderId;
+            int orderId;
      
             using (var connection = new SqlConnection(this.connectionString))
             {
-                // add customer to database
-                var insertCustomer = "INSERT INTO Customers (FirstName, LastName, Email, Phone, Street, PostalCode, City) VALUES (@firstName, @lastName, @email, @phone, @street, @postalCode, @city)";
-                var customerParameters = new { firstName = firstName, lastName = lastName, email = email, phone = phone, street = street, postalCode = postalCode, city = city };
-                connection.Execute(insertCustomer, customerParameters);
-
-                // add order to database
-                var insertOrder = "INSERT INTO Orders (CustomerId, OrderDate, OrderStatus) VALUES ((SELECT MAX(Id) FROM Customers), GETDATE(), 'new')";
-                connection.Execute(insertOrder);
-
-                // get products from cart
-                var query = "SELECT Carts.ProductId, Carts.Qty, Products.Price FROM Carts JOIN Products ON Carts.ProductId = Products.Id WHERE Carts.CartId = @cartId";
-                var queryParameters = new { cartId = cartId }; 
-                Products = connection.Query<CheckoutViewModel>(query, queryParameters).ToList();
-
-                // add order items to database
-                foreach (CheckoutViewModel CartItem in Products)
+                // Start a transaction
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                SqlTransaction transaction;
+                transaction = connection.BeginTransaction("checkoutTransaction");
+                command.Connection = connection;
+                command.Transaction = transaction;
+                try
                 {
-                    var insertOrderItem = "INSERT INTO OrderItems (OrderId, ProductId, Qty, Price) VALUES ((SELECT MAX(Id) FROM Orders), @productId, @qty, @price)";
-                    var orderItemParameters = new { productId = CartItem.ProductId, qty = CartItem.Qty, price = CartItem.Price };
-                    connection.Execute(insertOrderItem, orderItemParameters);
+                    // add customer to database
+                    command.CommandText = "INSERT INTO Customers (FirstName, LastName, Email, Phone, Street, PostalCode, City) VALUES (@firstName, @lastName, @email, @phone, @street, @postalCode, @city)";
+                    command.Parameters.AddWithValue("@firstName", firstName);
+                    command.Parameters.AddWithValue("@lastName", lastName);
+                    command.Parameters.AddWithValue("@email", email);
+                    command.Parameters.AddWithValue("@phone", phone);
+                    command.Parameters.AddWithValue("@street", street);
+                    command.Parameters.AddWithValue("@postalCode", postalCode);
+                    command.Parameters.AddWithValue("@city", city);
+                    command.ExecuteNonQuery();
 
-                    // update product stock
-                    var updateStock = "UPDATE Products SET Stock = Stock - @soldQty WHERE Id = @productId";
-                    var stockParameters = new { soldQty = CartItem.Qty, productId = CartItem.ProductId };
-                    connection.Execute(updateStock, stockParameters);
+                    // add order to database
+                    command.CommandText = "INSERT INTO Orders (CustomerId, OrderDate, OrderStatus) VALUES ((SELECT MAX(Id) FROM Customers), GETDATE(), 'new')";
+                    command.ExecuteNonQuery();
+
+                    // get products from cart
+                    var products = new List<CheckoutViewModel>();
+                    command.CommandText = "SELECT Carts.ProductId, Carts.Qty, Products.Price FROM Carts JOIN Products ON Carts.ProductId = Products.Id WHERE Carts.CartId = @cartId";
+                    command.Parameters.AddWithValue("@cartId", cartId);
+                    SqlDataReader productsReader = command.ExecuteReader();
+                    while (productsReader.Read())
+                    {
+                        var product = new CheckoutViewModel();
+                        product.ProductId = productsReader["ProductId"].ToString();
+                        product.Qty = Int32.Parse(productsReader["Qty"].ToString());
+                        product.Price = float.Parse(productsReader["Price"].ToString());
+                        products.Add(product);
+                  
+                    }
+                    productsReader.Close();
+
+                    // add order items to database
+                    foreach (CheckoutViewModel CartItem in products)
+                    {
+                        command.CommandText = "INSERT INTO OrderItems (OrderId, ProductId, Qty, Price) VALUES ((SELECT MAX(Id) FROM Orders), @productId, @qty, @price)";
+                        command.Parameters.AddWithValue("@productId", CartItem.ProductId);
+                        command.Parameters.AddWithValue("@qty", CartItem.Qty);
+                        command.Parameters.AddWithValue("@price", CartItem.Price);
+                        command.ExecuteNonQuery();
+
+                        // update product stock              
+                        command.CommandText = "UPDATE Products SET Stock = Stock - @soldQty WHERE Id = @productStockId";
+                        command.Parameters.AddWithValue("@soldQty", CartItem.Qty);
+                        command.Parameters.AddWithValue("@productStockId", CartItem.ProductId);
+                        command.ExecuteNonQuery();
+                    }
+                    // remove cart from database
+                    command.CommandText = "DELETE FROM Carts WHERE CartId = @cartIdToDelete";
+                    command.Parameters.AddWithValue("@cartIdToDelete", cartId);
+                    command.ExecuteNonQuery();
+
+                    transaction.Commit();
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Commit Exception Type: {0}", ex.GetType());
+                    Console.WriteLine("  Message: {0}", ex.Message);
 
-                // remove cart from database
-                var deleteCart = "DELETE FROM Carts WHERE CartId = @cartId";
-                var cartIdParameter = new { cartId = cartId };
-                connection.Execute(deleteCart, cartIdParameter);
-
-                // get order id for order confirmation
-                var getOrderId = "SELECT MAX(Id) FROM Orders";
-                OrderId = connection.QueryFirstOrDefault<int>(getOrderId);
+                    // Attempt to roll back the transaction.
+                    try
+                    {
+                        transaction.Rollback();
+                    }
+                    catch (Exception ex2)
+                    {
+                        // This catch block will handle any errors that may have occurred
+                        // on the server that would cause the rollback to fail, such as
+                        // a closed connection.
+                        Console.WriteLine("Rollback Exception Type: {0}", ex2.GetType());
+                        Console.WriteLine("  Message: {0}", ex2.Message);
+                    }              
+                }
             }
 
+            using (var connection = new SqlConnection(this.connectionString))
+            {
+                // get order id for order confirmation
+                var getOrderId = "SELECT MAX(Id) FROM Orders";
+                orderId = connection.Execute(getOrderId);
+            }
             // delete cart cookie
             var CartCookie = new HttpCookie("CartId");
             CartCookie.Expires = DateTime.Now.AddDays(-1);
             Response.Cookies.Add(CartCookie);
 
-            return RedirectToAction("ConfirmOrder", new { OrderId = OrderId });
+            return RedirectToAction("ConfirmOrder", new { OrderId = orderId });
         }
 
         [HttpGet]
